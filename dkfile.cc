e56ffaed4f163b9e866f3047feb0dd775763d4b9
root <root@localhost.localdomain>
2022-09-02 23:17:47 +0800

    cout和write函数不能混用？这是为什么？”

--- dkfile.cc
+++ dkfile.cc
@@ -7,6 +7,7 @@ int main(int argc, char **argv){
 	
 	rio_Readinitb(&rio, STDIN_FILENO);
 	while ((n = rio_Readlineb(&rio, buf, MAXLINE)) != 0 )
+		//cout << "接受到了一行数据!" <<  endl;
 		rio_Writen(STDOUT_FILENO, buf, n);
 	return 0;
 }
